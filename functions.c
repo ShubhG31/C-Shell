980253583efac00ad87a1a121245eff6fa52d3e1
shubhg3 <shubhg3@illinois.edu>
2021-11-23 00:07:52 -0600

    interpret() finished

--- functions.c
+++ functions.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 1024
+#define SIZE 1024       // Defines SIZE for standard input of 1024 characters, it is used for the commented out read() function
+#define WORD 128        // Defines WORD as the int of 128
+#define check " \t\r\n\a"
 /*
 This Code is an older implementation of getting input from the user and reallocates memory using the buffer
 */
@@ -60,6 +62,10 @@ char *read(){
         perror: prints a descriptive error message to stderr. First the string str is printed, 
                 followed by a colon then a space.
 
+        EXIT_SUCCESS: The value is 0, meanining the program is successful 
+
+        EXIT_FAILURE: The value is 8, meaning the program is unsuccessful
+
         */
         if(feof(stdin)){
             exit(EXIT_SUCCESS);
@@ -71,11 +77,45 @@ char *read(){
 return line;
 
 }
-char interpret(char* line){
+char **interpret(char* line){
+    int buffer=WORD;
+    int position=0;
+    char **words=malloc(WORD*sizeof(char*));
+    char *word;
+
+    //if words is NULL then allaocation of memory did not work
+    if(!words){
+        fprintf(stderr,"shell: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
     
+    // word stores the word found by the strtok which looks for certain key words inside "check" 's definiton
+    //strtok returns a pointer to the word
+    word=strtok(line,check);
+    while(word !=NULL){
+        words[position]=word;
+        position++;
+
+        if(position>=buffer){
+            buffer+=WORD;
+            words=realloc(words, buffer*sizeof(char*));
+            //if words is NULL then there was a memory allocation error in the realloc function call
+            if(!words){
+                fprintf(stderr,"shell: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        // Null in the function call tells it to use the same the string used earlier in the function call 
+        //with the same progress made in the last call
+        word=strtok(NULL, check);
+    }
+    words[position]=NULL;
+    return words;
+
+
 }
 
-int execute(int status){
+int execute(char **argument){
 
 
 }
--- functions.h
+++ functions.h
@@ -4,4 +4,4 @@ char read();
 
 char interpret(char *line);
 
-int execute(int status);
+int execute(char **argument);
