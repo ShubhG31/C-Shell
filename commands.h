e655779382aba070584dd4b4c11b4e3406f55cb6
shubhg3 <shubhg3@illinois.edu>
2021-12-25 07:40:13 -0600

    Update commands.h

--- commands.h
+++ commands.h
@@ -2,18 +2,18 @@ int shell_cd(char **argument);
 int ls_help(char **argument);
 int shell_exit(char **argument);
 
-// char *strings[]={
-//     "help",
-//     "cd",
-//     "exit"
-// };
+char *strings[]={
+    "help",
+    "cd",
+    "exit"
+};
 
-// char (*function[]) (char**)= {
-//     &shell_cd,
-//     &ls_help,
-//     &shell_exit
-// };
+char (*function[]) (char**)= {
+    &shell_cd,
+    &ls_help,
+    &shell_exit
+};
 
-// int number_calls(){
-//     return (sizeof(strings)/sizeof(char*));
-// }
+int number_calls(){
+    return (sizeof(strings)/sizeof(char*));
+}
