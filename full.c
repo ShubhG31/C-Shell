2e3aeb5587ee089c25d5a41b8d5f78ac2229210f
shubhg3 <shubhg3@illinois.edu>
2022-01-09 19:17:52 -0600

    All code: full project in file

--- full.c
+++ full.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 // #include "commands.h"
 // #include "functions.h"
 // #include "shell.h"
+
 #define SIZE 1024       // Defines SIZE for standard input of 1024 characters, it is used for the commented out read() function
 #define WORD 64        // Defines WORD as the int of 128
 #define check " \t\r\n\a"
